ad1d8dad7954545b69328342b04c95ef961a672b
yelizaveta-pystina <yelizavetapystina@gmail.com>
2025-10-04 18:29:41 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,18 @@
-package cs151.application;
+package cs151.spartantrack;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class Main extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/cs151/spartantrack/view/MainView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("SpartanTrack - Student Management");
         stage.setScene(scene);
         stage.show();
     }
