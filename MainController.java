6befab8404675c1c787829d73431de55e9295455
yelizaveta-pystina <yelizavetapystina@gmail.com>
2025-10-04 18:30:35 -0700

    Update MainController.java

--- MainController.java
+++ MainController.java
@@ -1,14 +1,15 @@
-package cs151.application;
+package cs151.spartantrack.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class MainController {
+
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Welcome to SpartanTrack!");
     }
 }
