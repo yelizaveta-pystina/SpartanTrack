2a5598e973b0a9425bc301a6834343fac0e9caa2
yelizaveta-pystina <yelizavetapystina@gmail.com>
2025-10-04 21:42:13 -0700

    Add files via upload

---
+++ ProgrammingLanguagesController.java
@@ -0,0 +1,14 @@
+package cs151.application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MainController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
